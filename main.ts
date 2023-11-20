4a44991d9ee7ec85841ae1e966a680f6a798e21a
TheOwlBear <145585663+TheOwlBear@users.noreply.github.com>
2023-11-20 10:22:11 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Surprised)
+    basic.showIcon(IconNames.Angry)
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
